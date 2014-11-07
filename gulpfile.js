77eb6665ac3daa56d1c0fdd4639411ca79f24bd8
Mario Uher <uher.mario@gmail.com>
2014-11-07 19:32:20 +0100

    Adds Windows too!

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var downloadAtomShell = require('gulp-download-atom-shell');
+var os = require('os');
 
-gulp.task('downloadAtomShell', function(callback) {
+var platform = os.platform();
+gulp.task('download-atom-shell', function(callback) {
   downloadAtomShell({
     version: '0.19.1',
-    outputDir: 'bin'
+    outputDir: __dirname + '/bin/' + platform
   }, callback);
 });
 
+var command = '';
+switch(platform) {
+  case 'darwin':
+    command = 'Atom.app/Contents/MacOS/Atom'
+    break;
+  case 'win32':
+    command = 'atom.exe'
+    break;
+}
+
 gulp.task('demo', shell.task([
-  __dirname + '/bin/Atom.app/Contents/MacOS/Atom .'
+  __dirname + '/bin/' + platform + '/' + command + ' .'
 ]));
